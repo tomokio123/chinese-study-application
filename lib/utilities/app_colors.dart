aa7ce2ca1a34ee8b218359e71971acfbaaf80b6c
t.fukuyama <t.fukuyama0123@gmail.com>
2023-03-16 17:37:48 +0800

    tutorialページ追加(flutter_overboard)による。AppColor追加、名前雑すぎるので後で変更しろ

--- lib/utilities/app_colors.dart
+++ lib/utilities/app_colors.dart
@@ -6,6 +6,8 @@ class AppColors{
   static const mainGreen = Color.fromRGBO(63, 185, 42, 1);
   static const mainWhite = Color.fromRGBO(255, 255, 255, 1);
   static const mainBlack = Color.fromRGBO(36, 35, 35, 1);
+  static const mainMainMainBlue = Color.fromRGBO(26, 161, 173, 1);
+  static const mainMainBlue = Color.fromRGBO(74, 137, 178, 1);
   static const mainBlue = Color.fromRGBO(106 ,140, 198, 1);
   static const subBlue = Color.fromRGBO(174, 192, 227, 1);
   static const subGreen = Color.fromRGBO(129, 214, 116, 1);
--- lib/view/main_screen/my_page/other_setting_page/tutorial_page.dart
+++ lib/view/main_screen/my_page/other_setting_page/tutorial_page.dart
@@ -1,21 +1,63 @@
-import 'package:chinese_study_applicaion/Utilities/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_overboard/flutter_overboard.dart';
+
+import '../../../../utilities/app_colors.dart';
 
 class TutorialPage extends StatelessWidget {
-  const TutorialPage({Key? key}) : super(key: key);
+  TutorialPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          iconTheme: IconThemeData(color: AppColors.mainBlue),
-          title: Text('TutorialPage',style: TextStyle(color: AppColors.mainBlue)),
-          backgroundColor: AppColors.mainWhite),
-      body: SafeArea(
-        child: Container(
-          child: Text('TutorialPage'),
-        ),
+      body: OverBoard(
+        pages: pages,
+        showBullets: true,
+        skipText: "スキップ",
+        nextText: "次へ",
+        finishText: "了解！",
+        skipCallback: () {
+          // when user select SKIP
+          Navigator.pop(context);
+        },
+        finishCallback: () {
+          // when user select NEXT
+          Navigator.pop(context);
+        },
       ),
     );
   }
+
+  final pages = [
+    PageModel(
+        color: AppColors.subBlue,
+        imageAssetPath: 'images/main_image.png',
+        title: '左右のスワイプ',
+        body: 'NEXTを押さなくても左右にスワイプすることで画面の切替が出来ます',
+        doAnimateImage: true),
+    PageModel(
+        color: AppColors.mainBlue,
+        imageAssetPath: 'images/main_image.png',
+        title: '文字を表示できます',
+        body: '細かい説明をbodyに指定して書くことが出来ます',
+        doAnimateImage: true),
+    PageModel(
+        color: AppColors.mainMainBlue,
+        imageAssetPath: 'images/main_image.png',
+        title: '左右のスワイプ',
+        body: 'NEXTを押さなくても左右にスワイプすることで画面の切替が出来ます',
+        doAnimateImage: true),
+    PageModel.withChild(
+        child: Padding(
+            padding: EdgeInsets.only(bottom: 25.0),
+            child: Text(
+              "さあ、始めましょう",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 32,
+              ),
+            )),
+        color: AppColors.mainMainMainBlue,
+        //color: const Color(0xFF5886d6),
+        doAnimateChild: true)
+  ];
 }
